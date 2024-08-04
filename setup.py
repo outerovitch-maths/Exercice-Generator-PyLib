7c86a4b33dad4c93deccaeaf3a068e313efe66f6
outerovitch-maths <outero.co@gmail.com>
2024-08-04 22:29:38 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Exercice-Math-Generator-PyLib',
+    version='0.1',
+    description='A simple Python library for creating random mathemaitical exercices.',
+    author='C Outerovitch',
+    packages=find_packages(),
+)
